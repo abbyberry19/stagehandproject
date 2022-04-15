6f8d7615487113b61fc013f8bcee4a9dd5e0e93b
Abby Berry <abbyberry100@gmail.com>
2022-04-15 13:53:57 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script>
-import SignupView from './components/SignupView.vue'
-import LoginView from './components/LoginView.vue'
-import DashboardView from './components/DashboardView.vue'
-import ProfileView from './components/ProfileView.vue'
-import CalendarView from './components/CalendarView.vue'
-import SettingsView from './components/SettingsView.vue'
-import NotFound from './components/NotFound.vue'
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
 
-const routes = {
-  '/dashboard': DashboardView,
-  '/signup': SignupView,
-  '/login': LoginView,
-  '/profile': ProfileView,
-  '/calendar': CalendarView,
-  '/settings': SettingsView
-  }
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  data() {
-    return {
-      currentPath: window.location.hash
-    }
+  name: 'App',
+
+  components: {
+    HelloWorld,
   },
-  computed: {
-    currentView() {
-      return routes[this.currentPath.slice(1) || '/'] || NotFound
-    }
-  },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-      this.currentPath = window.location.hash
-		})
-  }
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<template>
-  <a href="/dashboard">Dashboard</a> |
-  <a href="/signup">Signup</a> |
-  <a href="/login">Login</a> |
-  <a href="/profile">Profile</a> |
-  <a href="/calendar">Calendar</a> |
-  <a href="/settings">Settings</a> |
-  <a href="/non-existent-path">Not Found</a>
-  <component :is="currentView" />
-</template>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-import { createApp,h } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-//import Vue from 'vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import './style/style.css'
 
 loadFonts()
 
-createApp({
-    render: ()=>h(App)
-})
-.use(vuetify)
-.mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
